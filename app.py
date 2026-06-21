ce60cc1012c3857f4616baa0d7769fa3d09cda0c
hyunwoo91shb-sys <hyunwoo91.shb@gmail.com>
2026-06-21 23:18:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.runnables import RunnablePassthrough
 from dotenv import load_dotenv
